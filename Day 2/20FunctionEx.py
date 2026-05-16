89a5a3ef02efa07b016e6a52350646822f8ae00c
PrathmeshG-87 <pranaygumgaonkar@gmail.com>
2026-05-16 23:02:29 +0530

    Update

--- Day 2/20FunctionEx.py
+++
@@ -1,7 +0,0 @@
-ls=input("Enter Mobile Number: ")
-
-if len(ls) == 10 and ls.isdigit()==True and ls.startswith(("6","7","8","9")):
-    print("Mobile Number is valid in India")
-else:
-    print("Mobile Number is not valid in India")
-print(type(ls))
