fd14e3d6772be722cf3147eaa4215d3829cab820
PrathmeshG-87 <pranaygumgaonkar@gmail.com>
2026-05-20 15:33:07 +0530

    updated

---
+++ DSA/Day 8/4PowerRecursion.py
@@ -0,0 +1,15 @@
+# Find Power using Recursion -> x^y
+
+def power(x,y):
+    if y==0 or x==1:
+        return 1
+    elif y==1:
+        return x
+    elif x==0:
+        return 0
+    else:
+        return x*power(x,y-1)
+        
+if __name__=='__main__':
+    res=power(2,3)
+    print(res)
