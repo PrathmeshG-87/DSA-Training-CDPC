8ad13a7f11f5e460612a3e1b7a676aed96acb4aa
PrathmeshG-87 <pranaygumgaonkar@gmail.com>
2026-05-20 15:21:39 +0530

    updated

---
+++ DSA/Day 8/3MultiRecursion.py
@@ -0,0 +1,15 @@
+# Multiplication using Recursion
+
+def multiply(x,y):
+    if y==1:
+        return x
+    elif x==1:
+        return y
+    elif x==0 or y==0:
+        return 0
+    else:
+        return x+multiply(x,y-1)
+    
+if __name__=='__main__':
+    res=multiply(2,3)
+    print(res)
